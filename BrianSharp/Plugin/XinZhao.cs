cbe640377b8d4478e8ad42e9db7e4fb2fbf3840d
MissSecret <karenbaka123@hotmail.com>
2015-03-29 13:16:18 +0900

    Update XinZhao.cs

--- BrianSharp/Plugin/XinZhao.cs
+++ BrianSharp/Plugin/XinZhao.cs
@@ -87,7 +87,7 @@ namespace BrianSharp.Plugin
                 }
                 MainMenu.AddSubMenu(champMenu);
             }
-            Game.OnGameUpdate += OnGameUpdate;
+            Game.OnUpdate += OnGameUpdate;
             Drawing.OnDraw += OnDraw;
             Interrupter.OnPossibleToInterrupt += OnPossibleToInterrupt;
             Orbwalk.OnAttack += OnAttack;
